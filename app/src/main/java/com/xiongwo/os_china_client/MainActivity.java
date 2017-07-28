4bf8622dc5aae63116d715d4f17533fed4c117dd
Jae Xiong <byxiongwo@gmail>
2017-07-28 23:08:31 +0800

    初定Toolbar

--- app/src/main/java/com/xiongwo/os_china_client/MainActivity.java
+++ app/src/main/java/com/xiongwo/os_china_client/MainActivity.java
@@ -2,6 +2,11 @@ package com.xiongwo.os_china_client;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.tb_main);
+        ImageButton imageButton = (ImageButton) findViewById(R.id.ib_tool_bar_main);
+        imageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "侧滑！", Toast.LENGTH_SHORT).show();
+            }
+        });
+        toolbar.inflateMenu(R.menu.menu_toolbar_main);
+        toolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.action_search:
+                        Toast.makeText(MainActivity.this, "搜索！", Toast.LENGTH_SHORT).show();
+                        break;
+                }
+                return false;
+            }
+        });
     }
 }
